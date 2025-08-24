b7d59f3360c423dfe5bcd22fb476b1a53e21eebc
alain-mathy <alain_mathy@hotmail.com>
2025-08-24 11:05:17 +0200

    Add GetAllGames -> GameService

---
+++ BL_Crud_Server_Net8/Implementation/Interfaces/IGameService.cs
@@ -0,0 +1,9 @@
+﻿using BL_Crud_Server_Net8.Entities;
+
+namespace BL_Crud_Server_Net8.Implementation.Interfaces
+{
+    public interface IGameService
+    {
+        Task<List<Game>> GetAllGames();
+    }
+}
---
+++ BL_Crud_Server_Net8/Implementation/Services/GameService.cs
@@ -0,0 +1,22 @@
+﻿using BL_Crud_Server_Net8.Data;
+using BL_Crud_Server_Net8.Entities;
+using BL_Crud_Server_Net8.Implementation.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace BL_Crud_Server_Net8.Implementation.Services
+{
+    public class GameService : IGameService
+    {
+        private readonly AppDbContext _context;
+
+        public GameService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Game>> GetAllGames()
+        {
+            return await _context.Games.ToListAsync();
+        }
+    }
+}
