2a84c255795118a2461140a7d4e08a5588f2384e
alain-mathy <alain_mathy@hotmail.com>
2025-08-24 11:24:12 +0200

    Implement CRUD -> GameService

--- BL_Crud_Server_Net8/Implementation/Interfaces/IGameService.cs
+++ BL_Crud_Server_Net8/Implementation/Interfaces/IGameService.cs
@@ -5,5 +5,9 @@ namespace BL_Crud_Server_Net8.Implementation.Interfaces
     public interface IGameService
     {
         Task<List<Game>> GetAllGames();
+        Task<Game> GetGameById(int id);
+        Task CreateGame(Game game);
+        Task UpdateGame(int id, Game game);
+        Task DeleteGame(int id);
     }
 }
--- BL_Crud_Server_Net8/Implementation/Services/GameService.cs
+++ BL_Crud_Server_Net8/Implementation/Services/GameService.cs
@@ -14,9 +14,53 @@ namespace BL_Crud_Server_Net8.Implementation.Services
             _context = context;
         }
 
+        public async Task CreateGame(Game game)
+        {
+            _context.Games.Add(game);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteGame(int id)
+        {
+            var result = await _context.Games.FirstOrDefaultAsync(g => g.Id == id);
+            if (result is null)
+            {
+                throw new Exception("Game not found");
+            }
+
+            _context.Games.Remove(result);
+            await _context.SaveChangesAsync();
+        }
+
         public async Task<List<Game>> GetAllGames()
         {
             return await _context.Games.ToListAsync();
+        }
+
+        public async Task<Game> GetGameById(int id)
+        {
+            var result = await _context.Games.FirstOrDefaultAsync(g => g.Id == id);
+            if(result is null)
+            {
+                throw new Exception("Game not found");
+            }
+
+            return result;
+        }
+
+        public async Task UpdateGame(int id, Game game)
+        {
+            var result = await _context.Games.FirstOrDefaultAsync(g => g.Id == id);
+            if (result is null)
+            {
+                throw new Exception("Game not found");
+            }
+
+            result.Name = game.Name;
+            result.Developer = game.Developer;
+            result.Release = game.Release;
+
+            await _context.SaveChangesAsync();
         }
     }
 }
